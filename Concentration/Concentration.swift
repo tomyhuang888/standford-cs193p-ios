37aaffa2fd2645e88ad6dca423f101a0b33945df
Thomas Huang <thomasyh@uci.edu>
2018-04-16 16:25:26 -0700

    Lecture part 2 + gitignore file

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+class Concentration{
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int){
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
+                //check if no cards match
+                if cards[matchIndex].identifier == cards[index].identifier{
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                //either no card or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+//        if cards[index].isFaceUp {
+//            cards[index].isFaceUp = false
+//        } else {
+//            cards[index].isFaceUp = true
+//        }
+    }
+    
+    init(numberOfPairsOfCards: Int){
+        for _ in 0..<numberOfPairsOfCards{
+            let card = Card()
+            cards += [card,  card]
+        }
+        
+        //TODO: Shuffle the cards
+        
+    }
+}
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    lazy var game: Concentration = Concentration(numberOfPairsOfCards: (cardButtons.count+1)/2)
+    
     var flipCount : Int = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
@@ -20,15 +22,51 @@ class ViewController: UIViewController {
     
     @IBOutlet var cardButtons: [UIButton]!
     
-    var emojiChoices = ["🎃","👻","🎃","👻"]
+    //MARK: Handle Touch Card Behavior  
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
-            print("cardNumber = \(cardNumber)")
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+//            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
         } else {
             print("chosen card was not in cardButtons")
         }
+    }
+    
+    func updateViewFromModel(){
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if(card.isFaceUp){
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else{
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 0) : #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            }
+        }
+    }
+    
+    var emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+    
+//    var emoji = Dictionary<Int, String>
+    var emoji = [Int:String]()
+    
+    func emoji (for card: Card) -> String {
+        if emoji[card.identifier] == nil{
+            if emojiChoices.count > 0 {
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            }
+        }
+//        if emoji[card.identifier] != nil {
+//            return emoji[card.identifier]!
+//        }else{
+//            return "?"
+//        }
+        return emoji[card.identifier] ?? "?"
     }
     
     
@@ -41,7 +79,6 @@ class ViewController: UIViewController {
             button.setTitle(emoji,for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
-        flipCount += 1
     }
     
 }
